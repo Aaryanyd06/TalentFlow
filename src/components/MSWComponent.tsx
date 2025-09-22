a010a6047fec8a8c62f22ff3d73da5fa496ce2c6
Aaryanyd06 <aaryanyd2021@gmail.com>
2025-09-22 18:53:57 +0530

    seeded mock data in deployment

--- src/components/MSWComponent.tsx
+++ src/components/MSWComponent.tsx
@@ -2,21 +2,34 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { db } from "@/lib/db";
 
 export function MSWComponent({ children }: { children: React.ReactNode }) {
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
-    const initMSW = async () => {
+    const init = async () => {
+
       const { worker } = await import("@/mocks/browser");
       await worker.start();
-      setIsReady(true);
+
+      const jobCount = await db.jobs.count();
+
+
+      if (jobCount === 0) {
+        console.log("Database is empty. Seeding mock data...");
+        await fetch("/api/seed", { method: "POST" });
+        
+
+        window.location.reload();
+      } else {
+
+        setIsReady(true);
+      }
     };
 
-    if (!isReady) {
-      initMSW();
-    }
-  }, [isReady]);
+    init();
+  }, []);
 
   if (!isReady) {
     return null;
